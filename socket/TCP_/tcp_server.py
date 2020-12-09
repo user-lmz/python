a230f5e39af04113e842206f996b6b51e4abc9ed
lmz <mingzeluo888@gmail.com>
2020-12-09 15:45:07 +0800

    first commit

--- socket/TCP_/tcp_server.py
+++ socket/TCP_/tcp_server.py
@@ -1,14 +1,23 @@
 import  socket
 def main():
     tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_server_socket.bind(("", 8089))
+    tcp_server_socket.bind(("", 8090))
     tcp_server_socket.listen(128)
-    new_client_socket, client_addr = tcp_server_socket.accept()
-    print(client_addr)
-    recv_data = new_client_socket.recv(1024)
-    print(recv_data.decode("utf-8"))
-    new_client_socket.close()
-    tcp_server_socket.close()
+    while True:
+        print("等待一个新的客户到来...")
+        new_client_socket, client_addr = tcp_server_socket.accept()
+        print("一个新的客户已经到来:%s" % str(client_addr))
+        while True:
+            recv_data = new_client_socket.recv(1024)
+            print("客户发送过来的请求:%s" % recv_data.decode("gbk"))
+            if recv_data:
+                new_client_socket.send("我已经收到你的请求".encode("gbk"))
+            else:
+                break;
+
+        new_client_socket.close()
+        print("已经为客户端服务完毕")
+    #tcp_server_socket.close()
 
 if __name__ == '__main__':
     main()
